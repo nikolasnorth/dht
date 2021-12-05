9244614d65cf3d28cd1846ce6f6a425f837415e6
Nikolas North <nikolasn09@gmail.com>
2021-12-05 13:04:39 -0500

    use decimal string for representing Chord key

--- src/main/java/com/nikolasnorth/dht/Node.java
+++ src/main/java/com/nikolasnorth/dht/Node.java
@@ -3,37 +3,52 @@ package com.nikolasnorth.dht;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public final class Node<K, V> {
 
-  // Java's Object.hashCode() will be the consistent hash function used to create keys. The implementation of hashCode()
-  // returns a 32-bit integer, thus representing the number of bits in the keyspace.
-  private final static int m = 32;
-
-  private final int key;
+  // SHA-1 is the base hashing function for consistent hashing and maps to a keyspace of 160 bits.
+  private final static int m = 160;
 
   private final String ipAddress;
 
+  // Decimal string representation of an SHA-1 hash of the IP address.
+  private final String key;
+
   private final List<Resource<K, V>> storage;
 
-  private final List<FingerEntry<K, V>> finger;
+  private final List<Finger> finger;
 
-  private Node<K, V> predecessor;
+  private Finger predecessor;
 
   // Reference to the 0-th entry in the finger table
-  private FingerEntry<K, V> successor;
+  private Finger successor;
 
+  private static String hashOf(Object key) {
+
+    return "";
+  }
+
+  /**
+   * Create a new Chord ring.
+   *
+   * @param host host name
+   * @param port port number
+   */
   public Node(String host, int port) {
     this.ipAddress = String.format("%s:%d", host, port);
-    key = ipAddress.hashCode();
-    storage = new ArrayList<>();
-    finger = new ArrayList<>(m);
-    predecessor = null;
-    successor = null;
+    this.key = Util.sha1HashOf(this.ipAddress);
+    this.storage = new ArrayList<>();
+    this.finger = new ArrayList<>(m);
+    Finger toItself = new Finger(key, ipAddress);
+    for (int i = 0; i < m; i++) {
+      finger.add(i, toItself);
+    }
+    this.predecessor = toItself;
+    this.successor = this.finger.get(0);
 
     try {
       final HttpServer server = HttpServer.create(new InetSocketAddress(host, port), 0);
@@ -46,85 +61,91 @@ public final class Node<K, V> {
   }
 
   /**
-   * Creates a new node and adds it to the cluster. Returns the newly inserted node.
+   * Add a node to the Chord ring.
    *
-   * @param host New node's host
-   * @param port New node's port number
-   * @return Newly inserted node
+   * @param host     host name
+   * @param port     port number
+   * @param existing existing node in the Chord ring
    */
-  public Node<K, V> addNode(String host, int port) {
-    return new Node<>(host, port);
-  }
-
-  /**
-   * Returns the node that directly maps to the given key, or immediately succeeds it.
-   *
-   * @param key Chord key
-   * @return key's successor node
-   */
-  Node<K, V> findSuccessor(int key) {
-    return findPredecessor(key).successor.node;
-  }
-
-  /**
-   * Returns the node that immediately precedes the given key.
-   *
-   * @param key Chord key
-   * @return key's preceding node
-   */
-  Node<K, V> findPredecessor(int key) {
-    Node<K, V> prev = this;
-    while (Util.strictlyBetween(key, prev.key, prev.successor.id) || key == prev.successor.id) {
-      prev = prev.closestPrecedingFinger(key);
+  public Node(String host, int port, Node<K, V> existing) {
+    this.ipAddress = String.format("%s:%d", host, port);
+    this.key = Util.sha1HashOf(this.ipAddress);
+    this.storage = new ArrayList<>();
+    this.finger = new ArrayList<>(m);
+    Finger toItself = new Finger(key, ipAddress);
+    for (int i = 0; i < m; i++) {
+      finger.add(i, toItself);
     }
-    return prev;
+    this.predecessor = toItself;
+    this.successor = this.finger.get(0);
+
+    try {
+      final HttpServer server = HttpServer.create(new InetSocketAddress(host, port), 0);
+      server.start();
+      System.out.printf("Server listening on http://%s:%d%n", host, port);
+
+    } catch (IOException e) {
+      System.err.println("Could not start server.");
+    }
   }
 
   /**
-   * Returns the key's closest preceding node in the finger table.
+   * Returns the IP address of the node that directly maps to the given Chord key, or immediately succeeds it.
+   *
+   * @param key Chord key in decimal form
+   * @return IP address of successor node
+   */
+  public String findSuccessor(String key) {
+    BigInteger bigKey = new BigInteger(key), bigSuccessorKey = new BigInteger(successor.key);
+    if (Util.strictlyBetween(key, this.key, successor.key) || bigKey.compareTo(bigSuccessorKey) == 0) {  // key in range (this.key, successor.key]
+      return successor.ipAddress;
+    }
+    String closestPrecedingIpAddress = closestPrecedingFinger(key);
+    return null;
+  }
+
+  /**
+   * Returns the IP address for the node that immediately precedes the given Chord key.
    *
    * @param key Chord key
-   * @return id's closest preceding node in the finger table
+   * @return IP address for key's preceding node
    */
-  Node<K, V> closestPrecedingFinger(int key) {
+  public String findPredecessor(long key) {
+    return null;
+  }
+
+  /**
+   * Returns the IP address for the given Chord key's closest preceding node in the finger table.
+   *
+   * @param key Chord key
+   * @return ip address for the key's closest preceding node in the finger table
+   */
+  public String closestPrecedingFinger(String key) {
     for (int i = m - 1; i >= 0; i--) {
-      if (Util.strictlyBetween(finger.get(i).id, this.key, key)) {
-        return finger.get(i).node;
+      if (Util.strictlyBetween(finger.get(i).key, this.key, key)) {  // finger.key in range (this.key, key)
+        return finger.get(i).ipAddress;
       }
     }
-    return this;
-  }
-
-  void initFingerTable(Node<K, V> newNode) {
-
-  }
-
-  /**
-   * Returns the value stored at the given key. Returns null if the value does not exist.
-   *
-   * @param key Search key
-   * @return Value stored at key
-   */
-  Optional<V> get(String key) {
-    return Optional.empty();
-  }
-
-  /**
-   * Stores the given document in inside the distributed hash table. Overwrites any existing value stored at the given
-   * document key.
-   *
-   * @param document Document to be stored
-   */
-  void put(Resource<K, V> document) {
+    return this.ipAddress;
   }
 
   private final static class Resource<K, V> {
-    private K key;
-    private V value;
+    private final K key;
+    private final V value;
+
+    public Resource(K key, V value) {
+      this.key = key;
+      this.value = value;
+    }
   }
 
-  private final static class FingerEntry<K, V> {
-    private int id;
-    private Node<K, V> node;
+  private final static class Finger {
+    private final String key;
+    private final String ipAddress;
+
+    private Finger(String key, String ipAddress) {
+      this.key = key;
+      this.ipAddress = ipAddress;
+    }
   }
 }
--- src/main/java/com/nikolasnorth/dht/Util.java
+++ src/main/java/com/nikolasnorth/dht/Util.java
@@ -1,5 +1,10 @@
 package com.nikolasnorth.dht;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public final class Util {
 
   /**
@@ -9,13 +14,33 @@ public final class Util {
    * @param b Chord key
    * @return true iff strictly between, false otherwise
    */
-  public static boolean strictlyBetween(int key, int a, int b) {
-    if (a < b) {
-      return a < key && b >= key;
-    } else if (a > b) {
-      return a < key || b >= key;
+  public static boolean strictlyBetween(String key, String a, String b) {
+    BigInteger bigKey = new BigInteger(key);
+    BigInteger bigA = new BigInteger(a);
+    BigInteger bigB = new BigInteger(b);
+    if (bigA.compareTo(bigB) < 0) {
+      return bigA.compareTo(bigKey) < 0 && bigB.compareTo(bigKey) >= 0;
+    } else if (a.compareTo(b) > 0) {
+      return bigA.compareTo(bigKey) < 0 || bigB.compareTo(bigKey) >= 0;
     } else {
-      return a < key || a > key;
+      return bigA.compareTo(bigKey) != 0;
+    }
+  }
+
+  /**
+   * Returns the decimal string representation of an SHA-1 hash of the given key.
+   *
+   * @param key key to be hashed
+   * @return SHA-1 hash of key in decimal form
+   */
+  public static String sha1HashOf(String key) {
+    try {
+      MessageDigest msgDigest = MessageDigest.getInstance("SHA-1");
+      msgDigest.update(key.getBytes(StandardCharsets.UTF_8), 0, key.length());
+      return new BigInteger(msgDigest.digest()).toString();
+
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("Error creating SHA-1 hash.");
     }
   }
 }
